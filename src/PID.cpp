c3461d45412f745e08b262d46f0af8d756c2950b
AbhiB11 <abhishek.bichal@gmail.com>
2019-09-15 19:00:05 -0700

    Submitted Project

--- src/PID.cpp
+++ src/PID.cpp
@@ -4,7 +4,12 @@
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
 
-PID::PID() {}
+PID::PID() {
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+  cte_old = 0.0;
+}
 
 PID::~PID() {}
 
@@ -13,18 +18,24 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
 
+	Kp = Kp_;
+	Ki = Ki_;
+	Kd = Kd_;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+   d_error = Kd * cte - Kd * cte_old;
+   p_error  = Kp * cte;
+   i_error += (Ki * cte);
+   cte_old = cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return - p_error - i_error - d_error;  // TODO: Add your total error calc here!
 }
